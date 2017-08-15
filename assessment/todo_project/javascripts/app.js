ff1dd96e6296cf208dabef4a09f949930bdb8b9e
Tiffany Han <hantiffany@gmail.com>
2017-08-14 22:35:17 -0700

    namespace functions in TodoView, and more refactoring

--- assessment/todo_project/javascripts/app.js
+++ assessment/todo_project/javascripts/app.js
@@ -7,7 +7,7 @@ function serializeArrayToObj(array) {
 }
 
 var TodoList = {
-  dataPerMonth: function(array) {
+  returnDataPerMonth: function(array) {
     this.sortByDate(array);
     var dateString;
 
@@ -110,39 +110,159 @@ var Todo = {
 };
 
 var TodoView = {
-  templates: {},
+  templates: {
+    registerPartials: function() {
+      Handlebars.registerPartial('todos', $('[data-id=todos]').html());
+      Handlebars.registerPartial('todo', $('[data-id=todo]').html());
+    },
+    cacheTemplates: function() {
+      $('[type="text/x-handlebars"]').toArray().forEach(function(e) {
+        this.templates[$(e).data('id')] = Handlebars.compile($(e).html())
+      }, this);
+      this.templates.addForm = this.templates.form({action: 'add'});
+    },
+  },
+
+  todo: {
+    edit: function(source) {
+      var todo = this.todoList.get(Number(source.id));
+      source.date = this.todo.utility.makeDate.call(this, source)
+      todo.edit(source);
+    },
+    add: function(form) {
+      this.id++;
+      form.id = this.id;
+      form.date = this.todo.utility.makeDate.call(this, form);
+      var todo = Object.create(Todo).init(form);
+      this.todoList.add(todo);
+    },
+    utility: {
+      makeDate: function(form) {
+        if (form.month === 'Month' || form.year === 'Year') return;
+        var args = [Number(form.year), Number(form.month)];
+        if (form.day !== 'Day') args.push(Number(form.day));
+        return new Date(...args);
+      },
+    }
+  },
+
+  render: {
+    form: function($form) { $('#modal').html($form) },
+    main: function() {
+      $('nav .all').find('h2').trigger('click');
+    },
+    deletedTodo: function(id) {
+      $('#todos').find('[data-id=' + id + ']').remove();
+    },
+    editedTodo: function(id) {
+      $todo = $('#todos').find('[data-id=' + id + ']');
+      $todo.replaceWith(this.templates.todo(this.todoList.get(id)));
+    },
+    completedTodos: function(completed) {
+      completed = completed || this.todoList.completed;
+      this.todoList.sortByDate(completed);
+      var data = {completed: completed};
+      $('#todos').html(this.templates.todos(data));
+    },
+    allTodos: function(completed, uncompleted) {
+      completed = completed || this.todoList.completed;
+      uncompleted = uncompleted || this.todoList.uncompleted;
+      this.todoList.sortByDate(completed, uncompleted);
+      var data = {completed: completed, uncompleted: uncompleted};
+      $('#todos').html(this.templates.todos(data));
+    },
+    nav: function() {
+      var allPerMonth = this.todoList.returnDataPerMonth(this.todoList.all);
+      var completedPerMonth = this.todoList.returnDataPerMonth(this.todoList.completed);
+      this.render.utility.renderNavItems.call(this, allPerMonth, 'all');
+      this.render.utility.renderNavItems.call(this, completedPerMonth, 'completed');
+      $('.total-count').text(this.todoList.all.length);
+      $('.completed-count').text(this.todoList.completed.length);
+    },
+    selectValues: function(id) {
+      var todo = this.todoList.get(id);
+      if (!todo.date) return;
+      $('[name=day]').val(todo.date.getDate());
+      $('[name=month]').val(todo.date.getMonth());
+      $('[name=year]').val(todo.date.getFullYear());
+    },
+    utility : {
+      getFormHtml: function(id) {
+        var todo = this.todoList.get(id);
+        var data = Object.assign({}, todo, {action: 'edit'})
+        return this.templates.form(data);
+      },
+      renderNavItems: function(dataObj, which) {
+        var data = [];
+        for (key in dataObj) {
+          data.push({month: key, count: dataObj[key]});
+        }
+        $('#' + which).html(this.templates.navItems({months: data}));
+      },
+      persistSelected: function(cb) {
+        var $selected = $('nav').find('.selected');
+        var heading = $selected.data('heading');
+        var which = $selected.closest('section').attr('class');
+
+        cb();
+
+        var $target = $('nav .' + which).find('[data-heading="' +
+          heading + '"]');
+
+        if ($target.length)
+          return $target.trigger('click');
+        $('h1 .count').text('0');
+        this.render.allTodos.call(this, [], []);
+      },
+    }
+  },
+
+  toggle: {
+    modal: function(which) { $('#modal, #overlay').fadeToggle(which); },
+    completed: function(id) {
+      this.todoList.get(id).toggleCompleted();
+      this.todoList.updateCompleted(id);
+    },
+    selected: function($target) {
+      $('nav').find('.selected').removeClass('selected');
+      $target.addClass('selected');
+    },
+    heading: function(text, count) {
+      var $span = $('<span>', {class: 'count'});
+      $('main h1').html($span.text(count)).prepend(text);
+    },
+  },
+
   listeners: {
     add: function(e) {
       e.preventDefault();
-      this.insertForm($(this.templates.addForm));
-      this.fadeModal(true);
+      this.render.form.call(this, $(this.templates.addForm));
+      this.toggle.modal.call(this, true);
     },
     overlay: function(e) {
       e.preventDefault();
-      this.fadeModal(false);
+      this.toggle.modal.call(this, false);
     },
     submit: function(e) {
       e.preventDefault();
       var form = serializeArrayToObj($(e.target).serializeArray());
       var action = $(e.target).closest('form').data('action');
 
-      if (action === 'add') {
-        this.addTodo(form);
-        this.renderAllTodos();
-        this.renderNav();
-        this.renderTotalCounts();
-        $('nav .all').find('h2').trigger('click');
+      switch (action) {
+        case 'add':
+          this.todo.add.call(this, form);
+          this.render.nav.call(this);
+          this.render.main.call(this);
+          break;
+        case 'edit':
+          var cb = (function() {
+            this.todo.edit.call(this, form);
+            this.render.nav.call(this);
+          }).bind(this);
+          this.render.utility.persistSelected.call(this, cb);
       }
 
-      if (action === 'edit') {
-        this.persistSelected(function() {
-          this.editTodo(form);
-          this.renderNav();
-          this.renderTotalCounts();
-        }.bind(this));
-      }
-
-      this.fadeModal(false);
+      this.toggle.modal.call(this, false);
     },
     button: function(e) {
       e.preventDefault();
@@ -152,213 +272,101 @@ var TodoView = {
 
       var array = $(e.target).closest('form').serializeArray();
       var form = serializeArrayToObj(array);
-      if (this.todos.get(Number(form.id)).completed)
-        return this.fadeModal(false);
+      if (this.todoList.get(Number(form.id)).completed)
+        return this.toggle.modal.call(this, false);
 
       var $todo = $('#todos').find('[data-id=' + form.id + ']');
       $todo.find('label').trigger('click');
-      this.fadeModal(false);
+      this.toggle.modal.call(this, false);
     },
     check: function(e) {
       e.preventDefault();
       var id = $(e.target).closest('.todo').data('id');
-
-      this.persistSelected(function() {
-        this.toggleCompleted(id);
-        this.renderEditedTodo(id);
-        this.todos.updateCompleted(id);
-        this.renderNav();
-        this.renderTotalCounts();
-      }.bind(this));
+      var cb = (function() {
+        this.toggle.completed.call(this, id);
+        this.render.editedTodo.call(this, id);
+        this.render.nav.call(this);
+      }).bind(this);
+      this.render.utility.persistSelected.call(this, cb);
     },
     delete: function(e) {
       e.preventDefault();
       var id = $(e.target).closest('.todo').data('id');
-
-      this.persistSelected(function() {
-        this.todos.delete(id);
-        this.renderDeletedTodo(id);
-        this.renderNav();
-        this.renderTotalCounts();
-      }.bind(this));
+      var cb = (function() {
+        this.todoList.delete(id);
+        this.render.deletedTodo.call(this, id);
+        this.render.nav.call(this);
+      }).bind(this);
+      this.render.utility.persistSelected.call(this, cb);
     },
     name: function(e) {
       e.preventDefault();
       e.stopPropagation();
       var id = $(e.target).closest('.todo').data('id');
-      this.insertForm(this.getFormHtml(id));
-      this.setSelectValues(id);
-      this.fadeModal(true);
+      this.render.form.call(this, this.render.utility.getFormHtml.call(this, id));
+      this.render.selectValues.call(this, id);
+      this.toggle.modal.call(this, true);
     },
-    navItems: function(e) {
-      e.preventDefault();
-      var $li = $(e.target).closest('li');
-      var which = $li.closest('ul').attr('id');
-      var month = Number($li.text().slice(0, 2)) - 1;
-      var completed = this.todos.filterByMonth(this.todos.completed, month);
-      this.toggleSelected($li);
-      this.toggleHeading($li.data('heading'), $li.find('span').text());
-
-      switch (which) {
-        case 'all':
-          var uncompleted = this.todos.filterByMonth(this.todos.uncompleted, month);
-          return this.renderAllTodos(completed, uncompleted);
-        case 'completed':
-          return this.renderCompletedTodos(completed);
-      }
-    },
-    navHeadings: function(e) {
+    nav: function(e) {
       e.preventDefault();
       var $e = $(e.target);
-      var which = $e.closest('section').attr('class');
-      this.toggleSelected($e);
-      this.toggleHeading($e.data('heading'), $e.find('span').text());
+      var which = $e.parent().attr('class');
+      var completed;
+      var uncompleted;
+
+      if ($e.is(':not(h2)')) {
+        var $e = $e.closest('li');
+        var month = Number($e.text().slice(0, 2)) - 1;
+        var completed = this.todoList.filterByMonth(this.todoList.completed, month);
+        var uncompleted = this.todoList.filterByMonth(this.todoList.uncompleted, month);
+        which = $e.parent().attr('id');
+      }
+
+      this.toggle.selected.call(this, $e);
+      this.toggle.heading.call(this, $e.data('heading'), $e.find('span').text());
 
       switch (which) {
         case 'all':
-          return this.renderAllTodos();
+          return this.render.allTodos.call(this, completed, uncompleted);
         case 'completed':
-          return this.renderCompletedTodos();
+          return this.render.completedTodos.call(this, completed);
       }
     },
     unload: function(e) {
-      localStorage.setItem('todos', JSON.stringify(this.todos.all));
+      localStorage.setItem('todos', JSON.stringify(this.todoList.all));
     },
   },
-  registerPartials: function() {
-    Handlebars.registerPartial('todos', $('[data-id=todos]').html());
-    Handlebars.registerPartial('todo', $('[data-id=todo]').html());
-  },
-  cacheTemplates: function() {
-    $('[type="text/x-handlebars"]').toArray().forEach(function(e) {
-      this.templates[$(e).data('id')] = Handlebars.compile($(e).html())
-    }, this);
-    this.templates.addForm = this.templates.form({action: 'add'});
-  },
-  persistSelected: function(cb) {
-    var $selected = $('nav').find('.selected');
-    var heading = $selected.data('heading');
-    var which = $selected.closest('section').attr('class');
 
-    cb();
+  setup: {
+    restoreCachedTodos: function() {
+      var array = JSON.parse(localStorage.getItem('todos')) || [];
+      array.forEach(function(item) {
+        if (item.date) item.date = new Date(item.date);
+        this.todoList.add(Object.create(Todo).init(item));
+      }, this);
+    },
+    bind: function() {
+      $('#add').on('click', this.listeners.add.bind(this));
+      $('#overlay').on('click', this.listeners.overlay.bind(this));
+      $('#modal').on('submit', 'form', this.listeners.submit.bind(this));
+      $('#modal').on('click', '[type=button]', this.listeners.button.bind(this));
+      $('#todos').on('click', '.delete', this.listeners.delete.bind(this));
+      $('#todos').on('click', '.name', this.listeners.name.bind(this));
+      $('#todos').on('click', 'label', this.listeners.check.bind(this));
+      $('nav').on('click', 'h2, li', this.listeners.nav.bind(this));
+      $(window).on('unload', this.listeners.unload.bind(this));
+    },
+  },
 
-    var $target = $('nav .' + which).find('[data-heading="' +
-      heading + '"]');
-    if ($target.length)
-      return $target.trigger('click');
-    $('h1 .count').text('0');
-    this.renderAllTodos([], []);
-  },
-  fadeModal: function(which) { $('#modal, #overlay').fadeToggle(which); },
-  insertForm: function($form) { $('#modal').html($form) },
-  getFormHtml: function(id) {
-    var todo = this.todos.get(id);
-    var data = Object.assign({}, todo, {action: 'edit'})
-    return this.templates.form(data);
-  },
-  setSelectValues: function(id) {
-    var todo = this.todos.get(id);
-    if (!todo.date) return;
-    $('[name=day]').val(todo.date.getDate());
-    $('[name=month]').val(todo.date.getMonth());
-    $('[name=year]').val(todo.date.getFullYear());
-  },
-  makeDate: function(form) {
-    if (form.month === 'Month' || form.year === 'Year') return;
-    var args = [Number(form.year), Number(form.month)];
-    if (form.day !== 'Day') args.push(Number(form.day));
-    return new Date(...args);
-  },
-  renderTotalCounts: function() {
-    $('.total-count').text(this.todos.all.length);
-    $('.completed-count').text(this.todos.completed.length);
-  },
-  editTodo: function(source) {
-    var todo = this.todos.get(Number(source.id));
-    source.date = this.makeDate(source)
-    todo.edit(source);
-  },
-  addTodo: function(form) {
-    this.id++;
-    form.id = this.id;
-    form.date = this.makeDate(form);
-    var todo = Object.create(Todo).init(form);
-    this.todos.add(todo);
-  },
-  renderDeletedTodo: function(id) {
-    $('#todos').find('[data-id=' + id + ']').remove();
-  },
-  renderEditedTodo: function(id) {
-    $todo = $('#todos').find('[data-id=' + id + ']');
-    $todo.replaceWith(this.templates.todo(this.todos.get(id)));
-  },
-  toggleCompleted: function(id) {
-    this.todos.get(id).toggleCompleted();
-  },
-  toggleSelected: function($target) {
-    $('nav').find('.selected').removeClass('selected');
-    $target.addClass('selected');
-  },
-  toggleHeading: function(text, count) {
-    var $span = $('<span>', {class: 'count'});
-    $('main h1').html($span.text(count)).prepend(text);
-  },
-  renderCompletedTodos: function(completed) {
-    completed = completed || this.todos.completed;
-    this.todos.sortByDate(completed);
-    var data = {completed: completed};
-    $('#todos').html(this.templates.todos(data));
-  },
-  renderAllTodos: function(completed, uncompleted) {
-    completed = completed || this.todos.completed;
-    uncompleted = uncompleted || this.todos.uncompleted;
-    this.todos.sortByDate(completed, uncompleted);
-    var data = {completed: completed, uncompleted: uncompleted};
-    $('#todos').html(this.templates.todos(data));
-  },
-  renderNavItems: function(dataObj, which) {
-    var data = [];
-    for (key in dataObj) {
-      data.push({month: key, count: dataObj[key]});
-    }
-    $('#' + which).html(this.templates.navItems({months: data}));
-  },
-  renderNav: function() {
-    var allPerMonth = this.todos.dataPerMonth(this.todos.all);
-    var completedPerMonth = this.todos.dataPerMonth(this.todos.completed);
-    this.renderNavItems(allPerMonth, 'all');
-    this.renderNavItems(completedPerMonth, 'completed');
-  },
-  getCachedTodos: function() {
-    var array = JSON.parse(localStorage.getItem('todos')) || [];
-    array.forEach(function(item) {
-      if (item.date) item.date = new Date(item.date);
-      this.todos.add(Object.create(Todo).init(item));
-    }, this);
-  },
-  bind: function() {
-    $('#add').on('click', this.listeners.add.bind(this));
-    $('#overlay').on('click', this.listeners.overlay.bind(this));
-    $('#modal').on('submit', 'form', this.listeners.submit.bind(this));
-    $('#modal').on('click', '[type=button]', this.listeners.button.bind(this));
-    $('#todos').on('click', '.delete', this.listeners.delete.bind(this));
-    $('#todos').on('click', '.name', this.listeners.name.bind(this));
-    $('#todos').on('click', 'label', this.listeners.check.bind(this));
-    $('nav').on('click', 'li', this.listeners.navItems.bind(this));
-    $('nav h2').on('click', this.listeners.navHeadings.bind(this));
-    $(window).on('unload', this.listeners.unload.bind(this));
-  },
   init: function() {
-    this.registerPartials();
-    this.cacheTemplates();
-    this.bind();
-    this.todos = Object.create(TodoList).init();
-    this.id = this.todos.lastId();
-    this.getCachedTodos();
-    this.renderNav();
-    this.renderAllTodos();
-    this.renderTotalCounts();
-    $('nav .all').find('h2').trigger('click');
+    this.todoList = Object.create(TodoList).init();
+    this.setup.restoreCachedTodos.call(this);
+    this.id = this.todoList.lastId();
+    this.setup.bind.call(this);
+    this.templates.registerPartials.call(this);
+    this.templates.cacheTemplates.call(this);
+    this.render.nav.call(this);
+    this.render.main.call(this);
     return this;
   },
 };
